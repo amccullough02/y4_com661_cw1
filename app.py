7747b5f1998a88c3211bbe5bb544ae1ffa09ee52
Alex McCullough <alexmccullough@outlook.com>
2024-10-20 14:52:15 +0100

    feat: basic crud methods for star objects

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
 
@@ -26,6 +27,58 @@ def query_all_stars():
         data_to_return.append(body)
 
     return make_response(jsonify(data_to_return), 200)
+
+
+@app.route("/api/1.0/bodies/<string:s_id>", methods=["GET"])
+def query_one_star(s_id):
+    body = bodies.find_one({'_id': ObjectId(s_id)})
+    if body is not None:
+        body['_id'] = str(body['_id'])
+    return make_response(jsonify(body), 200)
+
+
+@app.route("/api/1.0/bodies", methods=["POST"])
+def add_star():
+    new_star = {
+        "name": request.form["name"],
+        "radius": request.form["radius"],
+        "mass": request.form["mass"],
+        "density": request.form["density"],
+        "surface_temperature": request.form["surface_temperature"],
+        "distance": request.form["distance"],
+        "spectral_classification": request.form["spectral_classification"],
+        "apparent_magnitude": request.form["apparent_magnitude"],
+        "absolute_magnitude": request.form["absolute_magnitude"],
+        "planets": []
+    }
+    new_star_id = bodies.insert_one(new_star)
+    new_star_link = "http://127.0.0.1:5000/api/v1.0/bodies/" + \
+        str(new_star_id.inserted_id)
+    return make_response(jsonify({"url": new_star_link}), 201)
+
+
+@app.route("/api/1.0/bodies/<string:s_id>", methods=["PUT"])
+def modify_star(s_id):
+    bodies.update_one(
+        {"_id": ObjectId(s_id)}, {"$set": {
+            "name": request.form["name"],
+            "radius": request.form["radius"],
+            "mass": request.form["mass"],
+            "density": request.form["density"],
+            "surface_temperature": request.form["surface_temperature"],
+            "distance": request.form["distance"],
+            "spectral_classification": request.form["spectral_classification"],
+            "apparent_magnitude": request.form["apparent_magnitude"],
+            "absolute_magnitude": request.form["absolute_magnitude"]
+        }})
+    edited_star_link = "http://127.0.0.1:5000/api/v1.0/bodies/" + s_id
+    return make_response(jsonify({"url": edited_star_link}), 200)
+
+
+@app.route("/api/1.0/bodies/<string:s_id>", methods=["DELETE"])
+def remove_star(s_id):
+    bodies.delete_one({"_id": ObjectId(s_id)})
+    return make_response(jsonify({}), 204)
 
 
 if __name__ == "__main__":
