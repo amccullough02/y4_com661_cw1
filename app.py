2d3ecab644bf2d4273bc59dd012e5ba3fff49562
Alex McCullough <alexmccullough@outlook.com>
2024-10-23 13:29:57 +0100

    feat: CRUD methods for planets

--- app.py
+++ app.py
@@ -79,19 +79,18 @@ def modify_star(s_id):
             "absolute_magnitude": request.form["absolute_magnitude"]
         }})
     edited_star_link = "http://127.0.0.1:5000/api/v1.0/bodies/" + s_id
-    # TODO: {} not returned
     return make_response(jsonify({"url": edited_star_link}), 200)
 
 
 @app.route("/api/v1.0/bodies/<string:s_id>", methods=["DELETE"])
 def remove_star(s_id):
     bodies.delete_one({"_id": ObjectId(s_id)})
-    return make_response(jsonify({}), 204)
+    return make_response(jsonify({}), 204)  # TODO: {} not returned
 
 
 # PLANETS
 
-@app.route("/api/v1.0/bodies<string:s_id>/planets", methods=["GET"])
+@app.route("/api/v1.0/bodies/<string:s_id>/planets", methods=["GET"])
 def query_all_planets(s_id):
     data_to_return = []
     body = bodies.find_one(
@@ -103,20 +102,70 @@ def query_all_planets(s_id):
     return make_response(jsonify(data_to_return), 200)
 
 
-def query_one_planet():
-    pass
+@app.route("/api/v1.0/bodies/<string:s_id>/planets/<string:p_id>",
+           methods=["GET"])
+def query_one_planet(s_id, p_id):
+    body = bodies.find_one(
+        {"planets._id": ObjectId(p_id)},
+        {"_id": 0, "planets.$": 1})
+    body["planets"][0]["_id"] = str(body["planets"][0]["_id"])
+    return make_response(jsonify(body["planets"][0]), 200)
 
 
-def add_planet():
-    pass
+@app.route("/api/v1.0/bodies/<string:s_id>/planets", methods=["POST"])
+def add_planet(s_id):
+    planet_to_add = {
+        "_id": ObjectId(),
+        "name": request.form["name"],
+        "radius": request.form["radius"],
+        "mass": request.form["mass"],
+        "density": request.form["density"],
+        "surface_temperature": request.form["surface_temperature"],
+        "apoapsis": request.form["apoapsis"],
+        "periapsis": request.form["periapsis"],
+        "eccentricity": request.form["eccentricity"],
+        "orbital_period": request.form["orbital_period"],
+        "status": request.form["status"],
+        "num_moons": request.form["num_moons"],
+        "contributed_by": request.form["contributed_by"]
+    }
+    bodies.update_one({"_id": ObjectId(s_id)}, {"$push":
+                                                {"planets": planet_to_add}})
+    new_planet_link = "http://127.0.0.1:5000/api/v1.0/bodies/" + \
+        s_id + "/planets/" + str(planet_to_add["_id"])
+    return make_response(jsonify({"url": new_planet_link}), 201)
 
 
-def modify_planet():
-    pass
+@app.route("/api/v1.0/bodies/<string:s_id>/planets/<string:p_id>",
+           methods=["PUT"])
+def modify_planet(s_id, p_id):
+    modified_planet = {
+        "planets.$.name": request.form["name"],
+        "planets.$.radius": request.form["radius"],
+        "planets.$.mass": request.form["mass"],
+        "planets.$.density": request.form["density"],
+        "planets.$.surface_temperature": request.form["surface_temperature"],
+        "planets.$.apoapsis": request.form["apoapsis"],
+        "planets.$.periapsis": request.form["periapsis"],
+        "planets.$.eccentricity": request.form["eccentricity"],
+        "planets.$.orbital_period": request.form["orbital_period"],
+        "planets.$.status": request.form["status"],
+        "planets.$.num_moons": request.form["num_moons"],
+        "planets.$.contributed_by": request.form["contributed_by"]
+    }
+    bodies.update_one({"planets._id": ObjectId(p_id)},
+                      {"$set": modified_planet})
+    modified_planet_url = "http://127.0.0.1:5000/api/v1.0/bodies/" + \
+        s_id + "/planets/" + p_id
+    return make_response(jsonify({"url": modified_planet_url}), 200)
 
 
-def remove_planet():
-    pass
+@app.route("/api/v1.0/bodies/<string:s_id>/planets/<string:p_id>",
+           methods=["DELETE"])
+def remove_planet(s_id, p_id):
+    bodies.update_one({"_id": ObjectId(s_id)}, {
+                      "$pull": {"planets": {"_id": ObjectId(p_id)}}})
+    return make_response(jsonify({}), 204)
 
 # AUTH
 
