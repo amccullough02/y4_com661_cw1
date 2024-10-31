288d8133eefffc63ae0654f98fbfe0290e37f9e7
Alex McCullough <alexmccullough@outlook.com>
2024-10-31 20:08:04 +0000

    feat: route to create admin accounts

--- blueprints/auth/auth.py
+++ blueprints/auth/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, make_response, jsonify
 from datetime import datetime, UTC, timedelta
 from jwt import encode
 from bcrypt import checkpw, gensalt, hashpw
-from decorators import jwt_required
+from decorators import jwt_required, admin_required
 from globals import db, secret_key
 
 auth_bp = Blueprint("auth_bp", __name__)
@@ -50,6 +50,50 @@ def register():
 
     return make_response(
         jsonify({"message": "account registration successful"}), 200)
+
+
+@jwt_required
+@admin_required
+@auth_bp.route("/api/v1.0/register_admin", methods=["POST"])
+def register_admin():
+    required_fields = ["username", "email", "password"]
+
+    missing_fields = [field for field in required_fields
+                      if not request.form.get(field)]
+
+    if missing_fields:
+        return make_response(jsonify(
+            {"error": f"missing fields: {",".join(missing_fields)}"}), 404)
+
+    username = request.form["username"]
+    email = request.form["email"]
+    forename = request.form["forename"]
+    surname = request.form["surname"]
+    password = request.form["password"].encode("utf-8")
+
+    if users.find_one({"username": username}):
+        return make_response(
+            jsonify({"error": "username already exists"}), 400)
+
+    if users.find_one({"email": email}):
+        return make_response(
+            jsonify({"error": "email already exists"}), 400)
+
+    password = hashpw(password, gensalt())
+
+    user_to_add = {
+        "username": username,
+        "email": email,
+        "forename": forename,
+        "surname": surname,
+        "password": password,
+        "is_admin": True
+    }
+
+    users.insert_one(user_to_add)
+
+    return make_response(
+        jsonify({"message": "admin account registration successful"}), 200)
 
 
 @auth_bp.route("/api/v1.0/login", methods=["GET"])
