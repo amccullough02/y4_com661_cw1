cfd67f6aa74e1e816bb140057dbe3cba95be99f6
Alex McCullough <alexmccullough@outlook.com>
2024-11-24 12:25:50 +0000

    feat: added cross origin resource sharing (CORS)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from blueprints.stars.stars import stars_bp
 from blueprints.planets.planets import planets_bp
 from blueprints.auth.auth import auth_bp
@@ -6,6 +7,7 @@ from blueprints.logs.logs import logs_bp
 from sys import exit
 
 app = Flask(__name__)
+CORS(app)
 app.config["SECRET_KEY"] = "thewitchofcolchis"
 app.register_blueprint(stars_bp)
 app.register_blueprint(planets_bp)
